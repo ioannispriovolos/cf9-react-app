90ccfcd20e0c6bbbe9baffe7bd1f0c29be69f672
ioannispriovolos <priovolosg@hotmail.com>
2026-06-02 16:14:38 +0300

    fix(ui): add z-index on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
 
     return (
         <>
-            <header className="bg-cf-dark-red fixed w-full">
+            <header className="bg-cf-dark-red fixed w-full z-50">
                 <div className="container mx-auto px-4 flex items-center justify-between">
                     <Link to="/">
                         <img src="https://codingfactory.aueb.gr/sites/all/themes/cf_theme/logo.png" alt="CF logo" className="my-4 h-16"/>
